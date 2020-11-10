210a870c95f503fc548ee35eb82137c728459f9f
huxu <huxu@didiglobal.com>
2020-11-10 17:26:08 +0800

    :wrench: codes

--- 2020/11/abc182/p5.cpp
+++ 2020/11/abc182/p5.cpp
@@ -1,4 +1,6 @@
-// Not submitted
+// WA
+// Bad Solution
+// Great Solution https://atcoder.jp/contests/abc182/submissions/17998964
 #include<bits/stdc++.h>
 using namespace std;
 const int N = 1510;
@@ -38,23 +40,52 @@ int main() {
             }
             if(g[r][c]!=0) continue;
             bool can = false;
-            cout<<"r:"<<r<<" c:"<<c<<endl;
+            //cout<<"r:"<<r<<" c:"<<c<<endl;
             // row
             int lebb, ribb, lebk, ribk;
-            lebb = lower_bound(rbb[r].begin(), rbb[r].end(), c) - rbb[r].begin();
-            ribb = lebb - 1;
-            ribk = lower_bound(rbk[r].begin(), rbk[r].end(), c) - rbk[r].begin();
+            int ribbv=-1, lebbv=-1, lebkv=-1, ribkv=-1;
+            ribb = upper_bound(rbb[r].begin(), rbb[r].end(), c) - rbb[r].begin();
+            lebb = ribb - 1;
+            if (ribb < rbb[r].size()) {
+                ribbv = rbb[r][ribb];
+            }
+            if (lebb >= 0) {
+                lebbv = rbb[r][lebb];
+            }
+            ribk = upper_bound(rbk[r].begin(), rbk[r].end(), c) - rbk[r].begin();
             lebk = ribk - 1;
-            if (lebb>lebk || ribb<ribk) can=true;
+            if (ribk < rbk[r].size()) {
+                ribkv = rbk[r][ribk];
+            }
+            if (lebk >= 0) {
+                lebkv = rbk[r][lebk];
+            }
+            if (lebbv != -1 && lebbv>lebkv) can=true;
+            if (ribbv != -1 && (ribbv<ribkv || ribkv ==-1)) can=true;
             // col
             int ubb, dbb, ubk, dbk;
-            dbb = lower_bound(cbb[c].begin(), cbb[c].end(), r) - cbb[c].begin();
+            int ubbv=-1, dbbv=-1, ubkv=-1, dbkv=-1;
+            dbb = upper_bound(cbb[c].begin(), cbb[c].end(), r) - cbb[c].begin();
+            if (dbb < cbb[c].size()) {
+                dbbv = cbb[c][dbb];
+            }
             ubb = dbb - 1;
-            dbk = lower_bound(cbk[c].begin(), cbk[c].end(), c) - cbk[c].begin();
+            if (ubb >= 0) {
+                ubbv = cbb[c][ubb];
+            }
+
+            dbk = upper_bound(cbk[c].begin(), cbk[c].end(), r) - cbk[c].begin();
+            if (dbk < cbk[c].size()) {
+                dbkv = cbk[c][dbk];
+            }
             ubk = dbk - 1;
-            if(ubb>ubk || dbb<dbk) can=true;
-            cout <<lebb<<" "<<ribb<<" "<<lebk<<" "<<ribk<<" "<<ubb<<" "<<dbb<<" "<<ubk<<" "<<dbk<<endl;
-            if(can) {/**cout << "r:" << r << " c:" << c << " +1" << endl;**/++cnt;}
+            if (ubk>=0) {
+                ubkv=cbk[c][ubk];
+            }
+            if (ubbv!=-1 && ubbv>ubkv) can=true;
+            if (dbbv!=-1 && (dbbv<dbkv || dbkv==-1)) can=true;
+            //cout <<lebbv<<" "<<ribbv<<" "<<lebkv<<" "<<ribkv<<" "<<ubbv<<" "<<dbbv<<" "<<ubkv<<" "<<dbkv<<endl;
+            if(can) {/**cout<<"++"<<endl;**/++cnt;}
         }
     }
     cout<<cnt<<endl;
